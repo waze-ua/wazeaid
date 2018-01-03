4cf9239b5535adec4834ae2ab24cb4bda0b80da6
Alexander Shashkevich <a.shashkevich@infomir.com>
2018-01-03 19:45:40 +0200

    First workable solution

---
+++ config.example.js
@@ -0,0 +1,29 @@
+'use strict';
+
+exports.scanner = {
+        url: "https://www.waze.com/row-rtserver/web/TGeoRSS?tk=community&atf=SOS&types=alerts&format=JSON&ma=600&mj=100&mu=100&left=23.910453&right=39.880017&bottom=46.053247&top=51.477441&_=1510071267374",
+        interval: 5000, // waze polling interval
+        expire: 86400,  // seconds to expire reported uuids
+        sostypes:{
+            "SOS_NO_FUEL": "нет топлива",
+            "SOS_FLAT_TIRE": "спущено колесо",
+            "SOS_BATTERY_ISSUE": "разрядился аккумулятор",
+            "SOS_MEDICAL_HELP": "медицинская помощь",
+            "SOS_OTHER": "другое"
+        }
+};
+
+exports.mailer = {
+        smtp: {
+            login: "user",
+            password: "password",
+            host: "smtp.gmail.com",
+            port: 465
+        },
+        message: {
+            from: "example@gmail.com",
+            to: "sos@sos-service.com",
+            subject: "Waze SOS",
+            template: "message.tpl",
+        }
+};
---
+++ message.example.tpl
@@ -0,0 +1,15 @@
+Внимание!
+
+В {{time}} поступило сообщение о помощи от пользователя навигационного сервиса Waze!
+
+<a href='https://www.waze.com/uk/livemap?zoom=15&lat={{latitude}}&lon={{longitude}}'>Координаты события</a>.
+
+Тип помощи: {{type}}
+Контактный номер: {{phone}}
+Сообщение:
+<blockquote>
+{{message}}
+</blockquote>
+
+С Уважением,
+<a href="https://fb.com/wazeukraine/">Украинское сообщество</a> навигационного приложения Waze.
